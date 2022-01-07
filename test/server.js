0837368c1412dc0272b5015dc4262cc9e6e99a7d
DevzCat <HajimeDevLife@gmail.com>
2022-01-07 21:23:26 +0700

    Update the source code.

--- test/server.js
+++ test/server.js
@@ -1,33 +1,31 @@
 "use strict";
 
+/*
+ * Import modules.
+ * http library.
+ * */
 const http = require("http");
-const NodeParse = require("../lib/nodeparse");
-const data = require("./data");
+const NodeParse = require("../lib/core/NodeParse");
 
+/* Declare variables */
+const port = process.env.PORT || 8080;
+
+
+/* Create server */
 const server = http.createServer(function (req, res) {
-	const nodeparse = new NodeParse(req, res);
-	nodeparse.init();
-
+	const nodeparse = new NodeParse(req);		
+	nodeparse.parseParams();
 	if (req.method === "GET") {
-		if (nodeparse.queries) {
-			console.log(nodeparse.queries);
-			console.log(nodeparse.params);
-		}
-		res.writeHead(200, { "Content-Type": "application/json" });
-		res.write(JSON.stringify(data));
-		res.end();
-	} 
-	/*
-	else if (req.method === "POST") {
-		console.log(await nodeparse.data);
+		console.log("params ", nodeparse.params);
+		console.log("queries ", nodeparse.queries);
 		res.write("OK!");
 		res.end();
 	}
-	*/
 });
 
-
-server.listen(8080, function (error) {
+/* Listening */
+server.listen(port, function (error) {
 	if (error) throw error;
-	console.log(`Server is running on port 8080.`);
-})
+	console.log(`Server is running on ${port}.`);
+	return;
+});
